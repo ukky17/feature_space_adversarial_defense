ac113fb82f8b8a52ceddb0eff545366897a8d79e
ukky17 <i.love.ny517@gmail.com>
2021-02-11 12:53:51 +0900

    modify for ResNet50

--- geometry.sh
+++ geometry.sh
@@ -1,9 +1,9 @@
 for s in input hidden
 do
-  CUDA_VISIBLE_DEVICES=0 python geometry.py --basenet VGG_stl \
-                                            --model_path model_pth/vgg_stl.pth \
-                                            --target_layer 27 \
-                                            --exp_name vgg16_test \
+  CUDA_VISIBLE_DEVICES=0 python geometry.py --basenet ResNet50_stl \
+                                            --model_path model_pth/resnet50_stl.pth \
+                                            --target_layer layer3 \
+                                            --exp_name resnet50_test \
                                             --n_data 8000 \
                                             --space ${s}
 done
