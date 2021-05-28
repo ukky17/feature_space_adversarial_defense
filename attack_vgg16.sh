e21b107aac1c3329f9b1e73a219fdfa8052cf735
ukky17 <i.love.ny517@gmail.com>
2021-05-28 20:33:00 +0900

    update hyperparameters

--- attack_vgg16.sh
+++ attack_vgg16.sh
@@ -6,7 +6,7 @@ CUDA_VISIBLE_DEVICES=0 python attack.py --dataset stl10 \
                                         --basenet VGG_stl \
                                         --model_path model_pth/vgg_stl.pth \
                                         --exp_name ${exp_name} \
-                                        --target_layer 27 \
+                                        --target_layer 20 \
                                         --epochs 1500 \
                                         --lambda_c 0.25 \
                                         --lambda_adv 0.25 \
